201d971600cb391ebe365a35a0e756b9bf003409
Ron Kurr <github@jvmguy.com>
2015-01-02 00:04:22 +0000

    Working towards getting a Spock-based unit test working.

--- src/test/groovy/demo/SomeUnitTest.groovy
+++ src/test/groovy/demo/SomeUnitTest.groovy
@@ -1,14 +1,17 @@
 package demo
 
+import groovy.util.logging.Slf4j
 import spock.lang.Specification
 
 /**
  * An example of a unit-level test.
  */
+@Slf4j
 class SomeUnitTest extends Specification {
 
     def 'exercise some math'() {
         given:
+        log.debug( 'running unit test' )
         def i = 10
 
         when:
